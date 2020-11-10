b1509d52ab0f5569d3db9896b3a9c0abe8b446c9
JC-labs <32711866+JC-labs@users.noreply.github.com>
2020-11-10 15:02:12 +0200

    Implement the counting exception manager template. (#4)
    * Implement the manager.
    
    * Disable 'MacOS' tests because of AppleClang version

--- include/exception_manager/exception_manager.hpp
+++ include/exception_manager/exception_manager.hpp
@@ -1,5 +1,31 @@
 #pragma once
+#include "exception_manager_interface.hpp"
 
 namespace qal {
-	bool hello();
+	template <typename ...supported_exception_types>
+	class counting_exception_manager : public four_level_exception_manager_interface<counting_exception_manager<supported_exception_types...>, supported_exception_types...> {
+		friend four_level_exception_manager_interface<counting_exception_manager<supported_exception_types...>, supported_exception_types...>;
+	public:
+		using four_level_exception_manager_interface<counting_exception_manager<supported_exception_types...>, supported_exception_types...>::four_level_exception_manager_interface;
+		inline explicit counting_exception_manager()
+			: four_level_exception_manager_interface<counting_exception_manager<supported_exception_types...>, supported_exception_types...>()
+			, counters{ 0u } {}
+
+		size_t get_counter(exception_level level) const {
+			std::shared_lock _(counter_mutex);
+			return counters[size_t(level)];
+		}
+		inline size_t operator[](exception_level level) const { return get_counter(level); }
+
+	protected:
+		template <typename exception_type>
+		void on_push(exception_type const &, exception_level const &level) {
+			std::scoped_lock _(counter_mutex);
+			++counters[size_t(level)];
+		}
+
+	protected:
+		std::array<size_t, size_t(exception_level::LAST) + 1> counters;
+		mutable std::shared_mutex counter_mutex;
+	};
 }
--- source/exception_manager/exception_manager.cpp
+++ source/exception_manager/exception_manager.cpp
@@ -1,7 +1,1 @@
 #include "exception_manager.hpp"
-
-#include <iostream>
-
-bool qal::hello() {
-	return bool(std::cout << "Hello from \"exception_manager\"\n");
-}
